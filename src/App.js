7576790a08637d051bab69ed3a98f0e3a6be9895
dhirajko <dhirajkoirala2013@gmail.com>
2020-10-04 18:43:53 +0300

    bug fix for inital form

--- src/App.js
+++ src/App.js
@@ -6,15 +6,43 @@ import Nav from "./component/Nav/nav";
 import './App.css';
 
 function App() {
-  const [showPlayerSetting, setShowPlayerSetting]=useState()
+  const [showPlayerSetting, setShowPlayerSetting]=useState(true)
 
   useEffect(()=>{
-    const  showForm= JSON.parse(localStorage.getItem('showForm'))
-    setShowPlayerSetting(showForm || false)
+    let loc= new Promise((resolve, reject)=>{
+      let show = localStorage.getItem('showForm')
+      if(show){
+        console.log(show);
+        return resolve(JSON.parse(show))
+       }
+       else{
+         reject(true)
+       }
+    })
+    loc.then(data=>{
+      console.log(data);
+      setShowPlayerSetting(data)
+    })
+    .catch((err)=>{
+      setShowPlayerSetting(err)
+    })
+
   },[])
+
+  const showForm = async()=>{
+    
+    const  showForm = await JSON.parse(localStorage.getItem('showForm'))
+    if(!showForm){
+      console.log('sssss');
+      setShowPlayerSetting(true)
+    }else{
+      setShowPlayerSetting(showForm)
+    }
+  }
 
   return (
     <div className="App">
+      <h1 onClick={()=>console.log(showPlayerSetting)}>test</h1>
        <SnackbarProvider>     
         <Nav/>
         <Players showPlayerSetting={showPlayerSetting} setShowPlayerSetting={setShowPlayerSetting}/>
